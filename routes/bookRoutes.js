a7f230f5ed56ae463a2c2934ef7aa52f1b960732
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-16 02:29:17 -0800

    Added outline for api book routes

---
+++ routes/bookRoutes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+
+  // Get all books
+  app.get('/books', (req, res) => {
+
+  })
+
+  // Get a book
+  app.get('/books/$id', (req, res) => {
+
+  })
+
+  // Add a book
+  app.post('/books', (req, res) => {
+
+  })
+
+  // Update a book
+  app.put('/books/$id', (req, res) => {
+
+  })
+
+  // Delete a book
+  app.delete('/books/$id', (req, res) => {
+    
+  })
+
+}
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
