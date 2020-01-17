c9ca47629f5b0a1c5d36bd9c21750467970b77f6
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-16 16:00:35 -0800

    Added book routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,28 +1,40 @@
+const { Book } = require('../models')
+
 module.exports = app => {
 
   // Get all books
   app.get('/books', (req, res) => {
-
+    Book.findAll() 
+      .then(books => res.json(books))
+      .catch(e => console.log(e))
   })
 
   // Get a book
   app.get('/books/$id', (req, res) => {
-
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.json(book))
+      .catch(e => console.log(e))
   })
 
   // Add a book
   app.post('/books', (req, res) => {
-
+    Book.create(req.body)
+      .then(book => res.json(book))
+      .catch(e => console.log(e))
   })
 
   // Update a book
   app.put('/books/$id', (req, res) => {
-
+    Book.updateOne({ _id: req.params.id }, req.body )
+      .then(() => res.sendStatus(200))
+      .catch(e => console.log(e))
   })
 
   // Delete a book
   app.delete('/books/$id', (req, res) => {
-    
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.sendStatus(200))
+      .catch(e => console.log(e))
   })
 
 }
