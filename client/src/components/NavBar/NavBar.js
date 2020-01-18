2797dbed242b6df986417ae98cc43d243d11cf45
Maverick <maverick.schirmer@gmail.com>
2020-01-17 22:01:52 -0800

    Fixed home buttons

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-
+import Drawer from '../Drawer'
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
