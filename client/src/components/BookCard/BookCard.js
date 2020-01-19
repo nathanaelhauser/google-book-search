abfc82e6170f332639227069e7ecde1892f91aea
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-18 23:35:28 -0800

    Added styling to SearchList.js

--- client/src/components/BookCard/BookCard.js
+++ client/src/components/BookCard/BookCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import Box from '@material-ui/core/Box'
@@ -10,7 +11,7 @@ import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles({
   card: {
-    margin: 30
+    // margin: 30
   },
   mediaBox: {
     display: 'flex',
@@ -25,32 +26,34 @@ const BookCard = props => {
   const classes = useStyles()
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        title={props.title}
-        subheader={props.publishedDate}
-      />
-      <Box className={classes.mediaBox}>
-        <img src={props.image} alt={props.title} />
-      </Box>
-      <CardContent>
-        <Typography 
-          className={classes.title} 
-          color="textSecondary"
-          component="h2">
+    <Grid item sm={12} md={6}>
+      <Card className={classes.card}>
+        <CardHeader
+          title={props.title}
+          subheader={props.publishedDate}
+        />
+        <Box className={classes.mediaBox}>
+          <img src={props.image} alt={props.title} />
+        </Box>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            component="h2">
             {props.authors.replace(',', ' • ')}
-        </Typography>
-        <Typography  
-          variant="body2"
-          component="p">
+          </Typography>
+          <Typography
+            variant="body2"
+            component="p">
             {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" size="small" href={props.link} color="primary">View</Button>
-        <Button variant="contained" size="small" color="secondary">{props.action}</Button>
-      </CardActions>
-    </Card>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" size="small" href={props.link} color="primary">View</Button>
+          <Button variant="contained" size="small" color="secondary">{props.action}</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   )
 }
 
--- client/src/components/SearchList/SearchList.js
+++ client/src/components/SearchList/SearchList.js
@@ -1,27 +1,39 @@
 import React, { useContext } from 'react'
+import { makeStyles } from '@material-ui/core'
 import SearchContext from '../../utils/SearchContext'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import BookCard from '../BookCard'
+import lightBlue from '@material-ui/core/colors/lightBlue'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: lightBlue
+  }
+}))
 
 const SearchList = () => {
-
+  const classes = useStyles()
   const { books } = useContext(SearchContext)
 
   return (
-    <Paper>
-      {
-        books.map((book, index) => 
-          <BookCard 
-            key={index}
-            title={book.title}
-            authors={book.authors ? book.authors.join(',') : ''}           
-            publishedDate={book.publishedDate}
-            description={book.description}
-            image={book.image}
-            link={book.link}
-            action="Save" />)
-      }
+    <Paper className={classes.root}>
+      <Grid container spacing={3}>
+        {
+          books.map((book, index) =>
+            <BookCard
+              key={index}
+              title={book.title}
+              authors={book.authors ? book.authors.join(',') : ''}
+              publishedDate={book.publishedDate}
+              description={book.description}
+              image={book.image}
+              link={book.link}
+              action="Save" />)
+        }
+      </Grid>
     </Paper>
   )
 }
