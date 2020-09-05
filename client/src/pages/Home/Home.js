2ad68a231d24dd48ed4cb69a73f7ec3663776dbe
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-09-05 02:18:16 -0700

    Chnaged container component for cards on Home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
@@ -32,51 +32,47 @@ const Home = _ => {
   return (
     <Grid container spacing={4} direction="row" justify="space-around" alignItems="center">
       <Grid item xs={12} sm={6} md={4} className={classes.buttonContainer}>
-        <Link to="/search">
-          <Button>
-            <Card className={classes.card}>
-              <CardMedia
-                component="img"
-                alt="Find Books"
-                height="150"
-                image={require('./books.jfif')}
-                title="Find Books"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2" align="center">
-                  FIND A NEW BOOK
+        <Button component={RouterLink} to="/search">
+          <Card className={classes.card}>
+            <CardMedia
+              component="img"
+              alt="Find Books"
+              height="150"
+              image={require('./books.jfif')}
+              title="Find Books"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2" align="center">
+                FIND A NEW BOOK
                 </Typography>
-                <Typography variant="body1" color="textSecondary" component="p" align="center">
-                  Click here to search for new books
+              <Typography variant="body1" color="textSecondary" component="p" align="center">
+                Click here to search for new books
                 </Typography>
-              </CardContent>
-            </Card>
-          </Button>
-        </Link>
+            </CardContent>
+          </Card>
+        </Button>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4} className={classes.buttonContainer}>
-        <Link to="/saved">
-          <Button>
-            <Card className={classes.card}>
-              <CardMedia
-                component="img"
-                alt="Saved Books"
-                height="150"
-                image={require('./savedbooks.jfif')}
-                title="Saved Books"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2" align="center">
-                  Your Saved Books
+        <Button component={RouterLink} to="/saved">
+          <Card className={classes.card}>
+            <CardMedia
+              component="img"
+              alt="Saved Books"
+              height="150"
+              image={require('./savedbooks.jfif')}
+              title="Saved Books"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2" align="center">
+                Your Saved Books
               </Typography>
-                <Typography variant="body1" color="textSecondary" component="p" align="center">
-                  Come see what books you saved
+              <Typography variant="body1" color="textSecondary" component="p" align="center">
+                Come see what books you saved
               </Typography>
-              </CardContent>
-            </Card>
-          </Button>
-        </Link>
+            </CardContent>
+          </Card>
+        </Button>
       </Grid>
     </Grid>
   )
